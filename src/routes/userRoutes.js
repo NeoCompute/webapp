4a3f96137c8fefbc8535ec0a54f9b05f3499871a
Akhilesh S Kumbar <akhilkumbar@outlook.com>
2024-10-07 21:50:38 -0400

    added middleware to route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,7 @@ const userController = require("../controllers/userController");
 const authenticate = require("../middlewares/basicAuthentication");
 const methodHandler = require("../middlewares/methodHandler");
 const validateJsonContentType = require("../middlewares/validateJsonContentType");
-
+const payloadChecker = require("../middlewares/payloadChecker");
 const router = express.Router();
 
 // Public Routes
@@ -12,7 +12,12 @@ router.post("/user", validateJsonContentType, userController.createUserInfo);
 router.use("/user/self", methodHandler(["GET", "PUT"]));
 
 // Protected Routes
-router.get("/user/self", authenticate, userController.getUserInfo);
+router.get(
+  "/user/self",
+  authenticate,
+  payloadChecker,
+  userController.getUserInfo
+);
 router.put(
   "/user/self",
   authenticate,
