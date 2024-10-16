b6db414acef0d2ac3b11075d7320420d6d35a2a1
Akhilesh S Kumbar <akhilkumbar@outlook.com>
2024-10-15 22:18:09 -0400

    updated ownership

--- resources/scripts/handle_src_code.sh
+++ resources/scripts/handle_src_code.sh
@@ -4,8 +4,8 @@ set -ex
 sudo apt-get install -y unzip
 sudo mkdir -p /home/csye6225/webapp
 sudo unzip -o /tmp/webapp.zip -d /home/csye6225/webapp
-sudo chown -R csye6225:csye6225 /home/csye6225/webapp
-sudo chmod -R 775 /home/csye6225/webapp
+sudo chown -R csye6225:csye6225 /home/csye6225
+sudo chmod -R 775 /home/csye6225
 cd /home/csye6225/webapp
 sudo npm install
 
