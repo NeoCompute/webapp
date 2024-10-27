234cb3e84999fa0c6bb5e35b77a47df4f338cde0
Akhilesh S Kumbar <akhilkumbar@outlook.com>
2024-10-27 15:13:39 -0400

    manually installing cloudwatch-agent

--- resources/scripts/setup_cloudwatch.sh
+++ resources/scripts/setup_cloudwatch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
-sudo apt-get install -y amazon-cloudwatch-agent
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a start
-echo "cloudwatc setup done"
+
+echo "CloudWatch setup done"
